cad6989ae9ba726424f7d247a68e7807ca25b4f8
Peter Lollo <peteralollo@gmail.com>
2015-12-09 17:28:49 -0800

    Complete stringifyJSON function up to unstringifiableValues

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,94 +3,28 @@
 
 // but you don't so you're going to write it from scratch:
 
-/*var stringifyJSON = function(obj) {
-  // your code goes here
-  var result = "";
-  var args = Array.prototype.slice.call(arguments);
-  for(var i = 0; i<args.length; i++) {
-  	var argItem = args[i];
-  	argItem.forEach(function(item) {
-  		if(typeof item === "number") {
-  			result = result + item;
-  		}
-  		result += item;
-  	})
-  }
-  return result;
-};*/
 
 
-/*=====================
-Attempt two
-====================*/
 
-/*var stringifyJSON = function(obj) {
-  // your code goes here
-  var result = "";
-  var args = Array.prototype.slice.call(arguments);
-  if(args.length<3) {
-    return result;
-  }
-  args.reduce(function(start, item){
-    if(typeof item === "object") {
-  	 for(var prop in item) {
-  		  result += prop + ":";
-  		  result += item[prop]
-  		  start[prop] = item[prop];
-      }
-  	}
-    else if(typeof item === "number") {
-      result += item;
-    }
-    else if(Array.isArray(item)) {
-      for(var i = 0; i<item.length; i++) {
-        result += item[i];
-      }
-    }
-  	return start;
-  }, {})
-  return result;
-};*/
-
-
-/*var stringifyJSON = function(obj) {
-  var result;
-  if(obj.length === 0) {
-    return;
-  }
-  var last = obj.pop();
-  if(typeof last === "number"){
-    result = last.toString();
-  }
-  return result + stringifyJSON(obj)
-}*/
-
-//TREAT OBJ LIke a string! get it to.string() asap
 
 var stringifyJSON = function(obj) {
   var result = '';
   if(obj === null) {
     return 'null'
   }
-
-
   else if(Array.isArray(obj)) {
     result += '['
-    if(obj.length>1) {
+    if(obj.length>0) {
       for(var i = 0; i<obj.length; i++) {
-        result += stringifyJSON(obj[i].toString());
+        result += stringifyJSON(obj[i]);
         if(i !== obj.length-1) {
           result += ',';
         }
       }
     }
-    else if(obj.length>0) {
-      result += stringifyJSON(obj.toString());
-    }
     result += ']'
     return result;
   }
-
   else if(!(Number.isNaN(parseInt(obj)))) {
      return obj.toString();
   }
@@ -100,6 +34,24 @@ var stringifyJSON = function(obj) {
     result += '\"';
     return result;
   }
-  
+  else if(typeof obj === "object") {
+    result += '{';
+    var count = 0;
+    for(var prop in obj) {
+      count++;
+    }
+    if(count>0) {
+      for(var prop in obj) {
+        count --;
+        result += stringifyJSON(prop) + ':';
+        result += stringifyJSON(obj[prop]);
+        if(count !== 0) {
+          result += ',';
+        }
+      }
+    }
+    result += '}'
+    return result;
+  }
   return obj.toString();
 };
