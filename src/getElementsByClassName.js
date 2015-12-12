7056ab477cf917b783229bc3be23fbafccb29fe4
Peter Lollo <peteralollo@gmail.com>
2015-12-11 16:03:56 -0800

    Get getElementsByClassName to function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,18 +4,71 @@
 // };
 
 // But instead we're going to implement it from scratch:
+/*var getElementsByClassName = function(className
+){
+  // your code here
+  var target = '.';
+  target += className;
+  var answer = [];
+  var firstSearch = document;
+  var count = 0;
+  function search (el) {
+
+    if(count === 0) {
+     var el = document;
+    }
+    else {
+      var tar = '';
+      tar += el; 
+    }
+    var found = $(el).find(target)[0];
+    if(found === undefined) {
+      return answer;
+    }
+    answer.push(found)
+    count++;
+    return search(found);
+  }
+  return search();
+};*/
+
+
 var getElementsByClassName = function(className
 ){
   // your code here
-  var result = [];
-  var targetStr = '';
-  targetStr += '.';
-  targetStr += className;
-  return targetStr;
-  if($('.className') === undefined) {
-  	return result;
+  var targetClass = '.';
+  targetClass += className;
+  var targClassWithout = '';
+  targClassWithout += className;
+  var answer = [];
+  var firstSearch = document;
+  var count = 0;
+  function search (el) {
+    if(count === 0) {
+     var el = document;
+    }
+    if($(el).hasClass(targClassWithout)) {
+      answer.push($(el)[0]);
+    }
+    count++;
+    var find = $(el).find(targetClass);
+    var found = $(el).find(targetClass)[0];
+    if(found === undefined) {
+      /*if(el.hasChildNodes()) {
+        var children = found.childNodes;
+        for(var i = 0; i < children.length; i++) {
+          search(children[i]);
+        }
+      }*/ 
+      //EITHER SEARCH THE FIND ARRAY OR THE NEXT() ARRAY FOR HASCLASS()
+      if($(el).next().length !==0) {
+        return search($(el).next());
+      }
+      return answer;
+    }
+    //answer.push(found)
+    //count++;
+    return search(found);
   }
-  result.push($('.className').NODE);
-  NODE.removeClass('.className');
-  return result.push(getElementsByClassName(className));
+  return search();
 };
